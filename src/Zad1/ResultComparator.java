9ba0c4e485c4ff73556490ced37df23183cdd86c
Tomek <kemot15@tlen.pl>
2019-08-29 11:50:31 +0200

    Zad 1 ver 4 poprawa ResultComparator

--- src/Zad1/ResultComparator.java
+++ src/Zad1/ResultComparator.java
@@ -5,17 +5,10 @@ import java.util.Comparator;
 public class ResultComparator implements Comparator<Player> {
     @Override
     public int compare(Player o1, Player o2) {
-        if (o1 == null && o2 == null)
-            return 0;
-        if (o1 ==null && o2 !=null)
-            return -1;
-        if (o1 !=null && o2 ==null)
-            return 1;
-        if (o1.getPoints() > o2.getPoints())
-            return 1;
-        if (o1.getPoints() < o2.getPoints())
-            return -1;
-        return  (o1.getFirstName().compareTo(o2.getFirstName()));
-
+        int pointsCompare = Integer.compare(o1.getPoints(), o2.getPoints());
+        if (pointsCompare == 0)
+            return (o1.getFirstName().compareTo(o2.getFirstName()));
+            else
+                return pointsCompare;
     }
 }
